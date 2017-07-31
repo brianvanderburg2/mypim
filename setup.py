8cebd33e2442875af83f325d9f9e19d6d709ee8a
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2017-07-31 18:50:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mrbavii_mypim',
+    version='0.1',
+    description='A simple web-based PIM',
+    url='',
+    author='Brian Allen Vanderburg II',
+    license='MIT',
+    packages=['mrbavii_mypim'],
+    zip_safe=False,
+)
