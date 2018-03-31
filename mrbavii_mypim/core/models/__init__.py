f6f3e6badbd9504da4f33fac1f3b004ce9fe3be7
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2018-03-30 23:57:24 -0400

    Models are sorted based on MODEL_DEPENDS

--- mrbavii_mypim/core/models/__init__.py
+++ mrbavii_mypim/core/models/__init__.py
@@ -5,6 +5,12 @@ __copyright__   =   "Copyright (C) 2017 Brian Allen Vanderburg II"
 __license__     =   "Apache License 2.0"
 
 
+__all__ = ["all_models", "models_map"]
+
+from mrbaviirc.util.sort import depends_sort
+
+from ..errors import Error
+
 from .model import Model
 
 # Import others in order for them to be registered.
@@ -17,4 +23,12 @@ all_models = (
     MainModel,
     NotesModel
 )
+
+models_map = { i.MODEL_NAME: i for i in all_models }
+
+# Update the all_models tuple to be sorted
+_depends = { i.MODEL_NAME : i.MODEL_DEPENDS for i in all_models }
+_sorted = depends_sort(_depends)
+
+all_models = tuple(models_map[i] for i in _sorted)
 
